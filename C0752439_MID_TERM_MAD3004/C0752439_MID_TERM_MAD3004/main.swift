85f54cad08c6c9e2ee4189c2ebb558a54b03c73e
parag <paraggarg66@yahoo.in>
2019-06-20 18:23:50 -0400

    Initial Commit

---
+++ C0752439_MID_TERM_MAD3004/C0752439_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0752439_MID_TERM_MAD3004
+//
+//  Created by Gurparkash Kaur on 2019-06-20.
+//  Copyright © 2019 Parag Garg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
